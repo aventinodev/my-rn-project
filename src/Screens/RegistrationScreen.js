06e01ab24972418f4990826c123e556ba0bc99cf
Olena Bondar <olena.bondar1060@gmail.com>
2023-04-05 00:15:03 +0300

    create new files

---
+++ src/Screens/RegistrationScreen.js
@@ -0,0 +1,19 @@
+// import React, { useState } from "react";
+
+// import {
+//   Text,
+//   TextInput,
+//   View,
+//   Alert,
+//   TouchableOpacity,
+//   KeyboardAvoidingView,
+//   Platform,
+// } from "react-native";
+
+// import { AntDesign } from "@expo/vector-icons";
+
+// import { stylesForm } from "./StylesForm";
+
+export const RegistrationScreen = () => {
+  console.log("registrationScreen");
+};
